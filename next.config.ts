6d470ee2ea874f6bf7da889e196c67ffd8687999
Anton <60476048+ASA-GH@users.noreply.github.com>
2026-09-12 02:14:21 +0700

    chore: next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,26 @@
 import { withWyw } from "@wyw-in-js/nextjs";
+import type { NextConfig } from "next";
 
-export default withWyw({
+const nextConfig: NextConfig = {
   reactCompiler: false,
-});
+  async headers() {
+    return [
+      {
+        source: "/videos/hero-video/:path*.m3u8",
+        headers: [
+          { key: "Content-Type", value: "application/vnd.apple.mpegurl" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+      {
+        source: "/videos/hero-video/:path*.m2ts",
+        headers: [
+          { key: "Content-Type", value: "video/mp2t" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ];
+  },
+};
+
+export default withWyw(nextConfig);
